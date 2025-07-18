968df9562d22dd17cd6de69626706ea955f1ce8d
Juan Carlos <jcromero909@misena.edu.co>
2025-07-18 09:45:21 -0500

    refactor: apply ParseUUIDPipe to product ID parameters for improved validation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -26,24 +27,20 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(
-      id as `${string}-${string}-${string}-${string}-${string}`,
-    );
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(
-      id as `${string}-${string}-${string}-${string}-${string}`,
-      updateProductDto,
-    );
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(
-      id as `${string}-${string}-${string}-${string}-${string}`,
-    );
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.remove(id);
   }
 }
